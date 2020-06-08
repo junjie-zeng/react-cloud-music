44f99547b2a18ae4df011827c3c50fe8349cd6e5
junjiezeng <junjie.zeng@focussend.com>
2020-06-08 11:44:06 +0800

    antd按需加载

--- conde-community/src/container/Hello.tsx
+++ conde-community/src/container/Hello.tsx
@@ -2,17 +2,15 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import {increment,decrement} from '../redux/actions'
 import {StoreState,StateType} from '../redux/state'
-// import { Button } from 'antd-mobile'
-//import Hello from './HelloUi'
+import { Button } from 'antd-mobile'
 import '../App.css'
-import 'antd-mobile/dist/antd-mobile.css';
+// import 'antd-mobile/dist/antd-mobile.css';
 
 export interface Props{
     name:string;
     num?:number;
     increment?:()=>any;
     decrement?:()=>any;
-    // Button:any;
 }
 
 class Hello extends React.Component<Props> {
@@ -29,11 +27,13 @@ class Hello extends React.Component<Props> {
                     hello,{name}
                 </div>
                 <div>
-                    <button onClick = {increment}>+</button>
-                    <button onClick = {decrement}>-</button>
+                    {/* <button onClick = {increment}>+</button> */}
+                    {/* <button onClick = {decrement}>-</button> */}
+                    <Button  onClick = {increment} >+</Button>
+                    <Button  onClick = {decrement} >-</Button>
                     {/* {getExclamationMarks(num)} */}
                     {num}
-                    {/* <Button></Button> */}
+                   
                 </div>
                 
                 
