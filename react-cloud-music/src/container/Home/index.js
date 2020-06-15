20f85ff633061299c7bfe41767683b1af59bb176
junjiezeng <junjie.zeng@focussend.com>
2020-06-15 14:27:55 +0800

    公共组件开发 Home

--- react-cloud-music/src/container/Home/index.js
+++ react-cloud-music/src/container/Home/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { renderRoutes } from 'react-router-config'
+import { Top ,Tab,TabItem} from './style'
+import { NavLink } from 'react-router-dom' // 利用navLink组件进行路由跳转
+
 function Home (props){
     const { route } = props
     console.log(route)
     return(
         <div>
-            主页面
+            <Top>
+                <span className="iconfont menu">&#xe65c;</span>
+                <span className="title">WebApp</span>
+                <span className="iconfont search">&#xe62b;</span>
+            </Top>
+            <Tab>
+                <NavLink to = '/recommend' activeClassName = 'selected'>
+                    <TabItem><span>推荐</span></TabItem>
+                </NavLink>
+                <NavLink to = '/singers' activeClassName = 'selected'>
+                    <TabItem><span>歌手</span></TabItem>
+                </NavLink>
+                <NavLink to = '/rank' activeClassName = 'selected'>
+                    <TabItem><span>等级</span></TabItem>
+                </NavLink>
+            </Tab>
+            
+            {/* renderRoutes 读取路由配置转化为 Route 标签 */}
             {renderRoutes (route.routes)}
         </div>
     )
