851c901f879015803850a83263d61cf16a65ec62
junjiezeng <junjie.zeng@focussend.com>
2020-06-15 14:51:06 +0800

    引入redux

--- react-cloud-music/src/App.js
+++ react-cloud-music/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from './style'
 import { renderRoutes } from 'react-router-config' //renderRoutes 读取路由配置转化为 Route 标签
+import store from './store'
 import routes from './routes'
 import { HashRouter } from 'react-router-dom'
 function App() {
   return (
-    <HashRouter>
-       <IconStyle/>
-       <GlobalStyle/>
-       { renderRoutes(routes) }
-    </HashRouter>
+    <Provider store = {store}>
+      <HashRouter>
+        <IconStyle/>
+        <GlobalStyle/>
+        { renderRoutes(routes) }
+      </HashRouter>
+    </Provider>
+    
   );
 }
 
