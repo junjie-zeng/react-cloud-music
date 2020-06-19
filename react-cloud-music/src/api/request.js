2119e757238fcdc5849ff9f0de9cc38cb2427598
junjiezeng <junjie.zeng@focussend.com>
2020-06-19 15:35:29 +0800

    获取歌曲接口

--- react-cloud-music/src/api/request.js
+++ react-cloud-music/src/api/request.js
@@ -7,3 +7,9 @@ export const getBannerRequest = () => axiosInstance.get ('/banner')
 
 // 获取推荐列表
 export const getRecommendListRequest = () => axiosInstance.get ('/personalized')
+
+// 获取热歌列表
+export const getHotSingerListRequest = (count)=>axiosInstance.get(`/top/artists?offset=${count}`)
+
+// 获取歌曲列表
+export const getSingerListRequest = (category,alpha,count)=>axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`)
