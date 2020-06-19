6b424a752e2a9acce960893e36b96b0ac466c1ec
junjiezeng <junjie.zeng@focussend.com>
2020-06-19 15:26:53 +0800

    歌手列表

--- react-cloud-music/src/container/Singers/index.js
+++ react-cloud-music/src/container/Singers/index.js
@@ -1,36 +1,73 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Horizen from '../../baseUI/horizen-item'
-import {categoryTypes,alphaTypes} from '../../api/config'
-import { NavContainer } from "./style";
-function Singers() { 
-    let [category,setCategory] = useState('')
-    let [alpha,setAlpha] = useState('')
+import { categoryTypes, alphaTypes } from '../../api/config'
+import Scorll from '../../baseUI/scroll'
+import { NavContainer,ListContainer,List,ListItem} from "./style";
+function Singers() {
+    let [category, setCategory] = useState('')
+    let [alpha, setAlpha] = useState('')
     // 分类事件
-    let handleUpdateCategory = (val)=>{
+    let handleUpdateCategory = (val) => {
         setCategory(val)
     }
     // 字母事件
-    let handleUpdateAlpha = (val)=>{
+    let handleUpdateAlpha = (val) => {
         setAlpha(val)
     }
 
-    return(
+
+    //mock 数据
+    const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => {
+        return {
+            picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+            name: "隔壁老樊",
+            accountId: 277313426,
+        }
+    });
+
+    // 渲染函数 返回歌手列表
+    const renderSingerList = () => {
+        return (
+            <List>
+                {
+                    singerList.map((item, index) => {
+                        return (
+                            <ListItem key={item.accountId + index}>
+                                <div className="img_wrapper">
+                                    <img src={`${item.picUrl}?param=300*300`} width="100%" height="100%" />
+
+                                </div>
+                                <span className="name">{item.name}</span>
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        )
+    }
+
+    return (
         <NavContainer>
-            <Horizen list = {categoryTypes} title={"分类 (默认热门):"} 
-            oldVal = {category} 
-            handleClick = {handleUpdateCategory}>
+            <Horizen list={categoryTypes} title={"分类 (默认热门):"}
+                oldVal={category}
+                handleClick={handleUpdateCategory}>
 
             </Horizen>
-            <Horizen list = {alphaTypes} title={"首字母:"} 
-            oldVal = {alpha} 
-            handleClick = {handleUpdateAlpha}>
-                
+            <Horizen list={alphaTypes} title={"首字母:"}
+                oldVal={alpha}
+                handleClick={handleUpdateAlpha}>
+
             </Horizen>
+            <ListContainer>
+                <Scorll>
+                    {renderSingerList()}
+                </Scorll>
+            </ListContainer>
         </NavContainer>
-        
+
     )
-    
+
 }
 
 export default React.memo(Singers)
@@ -119,7 +156,7 @@ export default React.memo(Singers)
     //                     </li>
     //                 ))
     //             }
-                
+
     //         </ul>
     //     </div>
     // )
